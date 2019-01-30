e04cbf3cb419da6a0f628c112ccdd48cc11e9949
Trabendo-daze <brendon.kaufman1@gmail.com>
2019-01-30 15:35:14 -0600

    init

---
+++ 01_scrape_hendon_mob.R
@@ -0,0 +1,389 @@
+
+
+# Library Loading/Introductory Information ---------------------------------------------
+
+
+library(rvest) #for html scraping
+library(stringr) #for string manipulation
+library(lubridate) #for date reading/manipulation
+library(tidyverse) #for pipe usage/other miscellaneous functions, just good to have
+library(janitor)
+
+currency_conversion_df <- read_csv("currency_conversion.csv")
+#Exchange rates based on the end of 2017
+
+country_currency_mapping <- read_csv("country_currency_mapping.csv")
+country_currency_mapping <- country_currency_mapping %>%
+  select(countries_cashed_in = Country, code = Code, everything()) %>%
+  left_join(currency_conversion_df) %>%
+  replace_na(list(rate = 1))
+
+#Information on using "selectorgadget" accessed with: 
+
+vignette("selectorgadget")
+
+#PURPOSE OF THIS SCRIPT
+
+#This script was created for scraping information off of the live poker database, thehendonmob.com.
+#The Hendon Mob is a website which keeps records for all live poker tournament cashes for players around the world
+#and is seen as the go-to resource for understanding a poker player's live poker tournament background. 
+
+#The website provides information for players such as their name, nationality, hometown, and any and all live tournament cashes 
+#along with event name, event date, placement, dollars earned.
+
+#By scraping this information from The Hendon Mob, we can perform analyses to answer questions such as:
+
+#Is there a relationship between live earnings and unique profile views?
+#What is the distribution of nationalities in the database?
+#Do players of different nationalities exhibit different live cash distributions?
+
+#This script contains functions for:
+#1. Reading a specific player's Hendon Mob profile into R/csv.
+#2. Creating a summary dataframe for as many Hendon Mob profiles as desired (each row is a player, 
+#   columns for limited summary information)
+
+
+
+# HTML Codes for Selector Gadget reading ----------------------------------
+
+# ".date" - date of tournament
+# ".currency+ .currency" - List of cashes
+# ".currency" - List of cashes (alternate)
+# "td.place" - place in tournament
+# "event_name a" - event name
+# ".popularity" - unique views
+# ".player-profile-info-total-live__value" - total cashes
+# ".text-wrap""- hometown
+# ".player-profile-layout__name" - name and nationality
+
+
+# Functions for creating dataframe for a specific poker player/summary dataframe of entire database ---------------
+
+make_hendon_df <- function(url){
+  
+  #reads in url
+  url <- read_html(url) 
+  
+  #reads in date column
+  date <- url %>%  
+    html_nodes(".date") %>%
+    html_text() %>%
+    parse_date_time(c("dmy", "my"))
+  
+  #reads in cashes column
+  #there are two possibilities for the cashes html code depending on if the player cashed an event which is not in USD
+  #the function deals with this by first seeing if there are events in non-USD currencies, and if there aren't it uses
+  #an alternate html code
+  
+  cashes_double_currency <- url %>%
+    html_nodes(".currency+ .currency") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[$]" , "") %>%
+    str_replace_all("[,]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  cashes_single_currency <- url %>%
+    html_nodes(".currency") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[$]" , "") %>%
+    str_replace_all("[,]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  #only uses the alternate code if the first cashes code doesn't work (i.e. length of the associated vector == 0)
+  
+  if (length(cashes_double_currency > 0)) {
+    cashes <- cashes_double_currency
+  } else {
+    cashes <- cashes_single_currency
+  }
+  
+  #reads in placement column 
+  
+  placement <- url %>%
+    html_nodes("td.place") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[st]" , "") %>%
+    str_replace_all("[th]" , "") %>%
+    str_replace_all("[nd]" , "") %>%
+    str_replace_all("[rd]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  #reads in event name column, adjusts because there are extra blank events that get added because of html weirdness
+  
+  event_name <- url %>%
+    html_nodes(".event_name a") %>%
+    html_text()
+  
+  event_name <- event_name[!event_name == ""]
+  
+  #reads in hometown
+  
+  hometown <- url %>%
+    html_nodes(".text-wrap") %>%
+    html_text()
+  
+  #reads in name and country which come as a package in one html code
+  #splits the name and country apart and stores them separately
+  
+  name_and_country <- url %>%
+    html_nodes(".player-profile-layout__name") %>%
+    html_text() %>% 
+    str_replace_all("[\r\n]" , "") %>%
+    str_trim()
+  
+  name_and_country <- unlist(strsplit(name_and_country, split="              "))
+  name_and_country <- str_trim(name_and_country)
+  
+  nationality <- name_and_country[1]
+  name <- name_and_country[2]
+  
+  #reads in unique_views, does some separating since unique_view html code also includes the player name
+  
+  popularity_html <- url %>%
+    html_nodes(".popularity") %>%
+    html_text() %>% 
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[,]" , "") %>%
+    str_trim()
+  
+  popularity_list <- unlist(strsplit(popularity_html, split="              "))
+  popularity_list <- str_trim(popularity_list)
+  
+  unique_views <- as.numeric(popularity_list[2])
+  
+  #produces the dataframe for output, assigns a 0 to any events which don't have a cash listed
+  
+  cashes_df <- data.frame(name, nationality, unique_views, date, event_name, cashes, placement, stringsAsFactors = FALSE)
+  cashes_df <- cashes_df %>%
+    replace_na(list(cashes = 0))
+  
+  cashes_df
+}
+
+make_hendon_summary <- function(url){
+  
+  #this function does a lot which is similar to the previous function,
+  #but attempts to only offer a summary for each player, intended for input into a larger summary dataframe
+  
+  url <- read_html(url)
+  
+  name_and_country <- url %>%
+    html_nodes(".player-profile-layout__name") %>%
+    html_text() %>% 
+    str_replace_all("[\r\n]" , "") %>%
+    str_trim()
+  
+  name_and_country <- unlist(strsplit(name_and_country, split="              "))
+  name_and_country <- str_trim(name_and_country)
+  
+  nationality <- name_and_country[1]
+  name <- name_and_country[2]
+  
+  sum_of_cashes <- url %>%
+    html_nodes(".player-profile-info-total-live__value") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "")  %>%
+    str_replace_all("[,]" , "") %>%
+    str_replace_all("[$]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  popularity_html <- url %>%
+    html_nodes(".popularity") %>%
+    html_text() %>% 
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[,]" , "") %>%
+    str_trim()
+  
+  popularity_list <- unlist(strsplit(popularity_html, split="              "))
+  popularity_list <- str_trim(popularity_list)
+  
+  unique_views <- as.numeric(popularity_list[2])
+  
+  cashes_double_currency <- url %>%
+    html_nodes(".currency+ .currency") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[$]" , "") %>%
+    str_replace_all("[,]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  cashes_single_currency <- url %>%
+    html_nodes(".currency") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[$]" , "") %>%
+    str_replace_all("[,]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  #only uses the alternate code if the first cashes code doesn't work (i.e. length of the associated vector == 0)
+  
+  if (length(cashes_double_currency > 0)) {
+    cashes <- cashes_double_currency
+  } else {
+    cashes <- cashes_single_currency
+  }
+  
+  
+  #cashes is a vector of the cashes and we can determine number of binks from them
+  #binks should be defined here.
+  
+  number_of_cashes <- length(cashes)
+  
+  placement <- url %>%
+    html_nodes("td.place") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_replace_all("[st]" , "") %>%
+    str_replace_all("[th]" , "") %>%
+    str_replace_all("[nd]" , "") %>%
+    str_replace_all("[rd]" , "") %>%
+    str_trim() %>%
+    as.numeric()
+  
+  average_placement <- mean(placement)
+  
+  
+  date <- url %>%  
+    html_nodes(".date") %>%
+    html_text() %>%
+    parse_date_time(c("dmy", "my"))
+  
+  date_sorted <- sort(date)
+  first_date <- date_sorted[1]
+  last_date <- tail(date_sorted, n = 1)
+  
+  
+  countries_cashed_in <- url %>%  
+    html_nodes(".venue_flag") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_trim() 
+  
+  
+  number_of_countries_cashed <- length(unique(countries_cashed_in))
+  
+  event_names <- url %>%  
+    html_nodes(".event_name a") %>%
+    html_text() %>%
+    str_replace_all("[\r\n]" , "") %>%
+    str_trim() 
+  
+  event_names <- event_names[event_names != ""]
+  buy_in_df <- tibble(event_names, countries_cashed_in, cashes)
+  buy_in_df <- buy_in_df %>%
+    mutate(buy_in = parse_number(event_names)) %>%
+    left_join(country_currency_mapping) %>%
+    replace_na(list(buy_in = 0, code = "USD", rate = 1)) %>%
+    select(countries_cashed_in, buy_in, cashes, code, rate) %>%
+    mutate(buy_in_usd = buy_in/rate) %>%
+    mutate(buy_in_usd = replace(buy_in_usd, buy_in_usd > cashes, cashes/10))
+  
+  
+  #if buy-in is greater than or equal to cash, then buy-in = 1/10 of cash
+  
+  
+  average_buy_in <- mean(buy_in_df$buy_in_usd, na.rm = TRUE)
+  
+  bink_threshold <- 20 * average_buy_in
+  
+  number_of_binks <- sum(cashes > bink_threshold, na.rm = TRUE)
+  
+  hendon_summary <- data.frame(name, nationality, average_buy_in, number_of_cashes, 
+                               sum_of_cashes, average_placement, number_of_binks,
+                               number_of_countries_cashed, first_date, last_date, 
+                               unique_views, stringsAsFactors = FALSE)
+  
+  hendon_summary
+  
+
+}
+
+# Creating Entire Database ------------------------------------------------
+
+#this function compiles summaries using the make_hendon_summary function
+#then binds them all together in one dataframe.
+
+#the one variable for this function is "n" or the number of randomly selected summaries.
+
+#note the try statement in this function which was created because some links will return empty sites.
+#the code will let you know about the missing profile numbers but will proceed on.
+#if you'd like to turn these errors off, you can add the parameter "silent = TRUE" to the try statement.
+
+compile_hendon_summaries <- function(n){
+  
+  hendon_website_root <- "http://pokerdb.thehendonmob.com/player.php?a=r&n="
+  
+  hendon_total_df <- data_frame()
+  
+  random_hendon_vector <- sample(1:630000,n,replace=F)
+  
+  for (x in random_hendon_vector) {
+    temp_hendon_url <- paste(hendon_website_root,x, sep = "")
+    try(temp_hendon_df <- make_hendon_summary(temp_hendon_url))
+    hendon_total_df <- bind_rows(temp_hendon_df, hendon_total_df)
+    
+  } 
+  
+  hendon_total_df <- distinct(hendon_total_df)
+  
+  hendon_total_df
+  
+}
+
+
+
+# Creating and saving CSVs ------------------------------------------------
+
+hendon_summary_csv <- compile_hendon_summaries(1000)
+
+write_csv(hendon_summary_csv, path = "hendon_summaries.csv")
+
+
+
+# Testing Section ---------------------------------------------------------
+
+#Section for testing/outputting summaries for analysis. 
+
+
+#Things to add to summary data frame
+
+
+
+url <- "http://pokerdb.thehendonmob.com/player.php?a=r&n=64849"
+
+url <- read_html(url)
+
+
+
+
+test <- make_hendon_summary(url)
+
+#We want a dataframe with the event name and the country played in
+#We want to make columns with the buy-in and the currency of that country
+#buy-in comes from parse_number of the event name
+#currency comes from a join between the country_currency df and the country
+#then we create a mutate to convert into USD, if not USD already
+#then we get average buy-in and bink threshold 
+
+event_names <- event_names[event_names != ""]
+buy_in_df <- tibble(event_names, countries_cashed_in, cashes)
+buy_in_df <- buy_in_df %>%
+  mutate(buy_in = parse_number(event_names)) %>%
+  left_join(country_currency_mapping) %>%
+  replace_na(list(buy_in = 0, code = "USD", rate = 1)) %>%
+  select(countries_cashed_in, buy_in, cashes, code, rate) %>%
+  mutate(buy_in_usd = buy_in/rate) %>%
+  mutate(buy_in_usd = replace(buy_in_usd, buy_in_usd > cashes, cashes/10))
+
+average_buy_in <- mean(buy_in_df$buy_in_usd, na.rm = TRUE)
+
+
---
+++ 02_analyze_hendon_mob.R
@@ -0,0 +1,362 @@
+
+# Data and Library Load ---------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+
+#Distribution of countries in DB
+#Distribution of total earnings
+#Distribution of number of outliers
+#Distribution of average time between cashes
+#Distribution of time between outlier and next cash
+#Buy-ins? (This is difficult with Hendon Mob)
+#Cashes/year
+#What can we say and what can we not say with Hendon Mob Data??
+
+hendon_summaries_df <- read_csv("hendon_summaries.csv")
+
+hendon_summaries_df <- hendon_summaries_df %>%
+  mutate(new_name = case_when(is.na(name) == TRUE ~ nationality, is.na(name) == FALSE ~ name)) %>%
+  mutate(new_nationality = case_when(is.na(name) == TRUE ~ "Unlisted", is.na(name) == FALSE ~ nationality)) %>%
+  mutate(average_cash = round(sum_of_cashes/number_of_cashes, 2)) %>%
+  mutate(average_placement = round(average_placement), 2) %>%
+  mutate(average_buy_in = round(average_buy_in), 2) %>%
+  mutate(last_date = as_date(last_date)) %>%
+  mutate(first_date = as_date(first_date)) %>%
+  mutate(years_played = round((last_date - first_date)/365.25,2)) %>%
+  mutate(average_time_btwn_cash = round((last_date - first_date)/number_of_cashes), 2) %>%
+  mutate(average_time_btwn_cash = as.numeric(average_time_btwn_cash)) %>%
+  mutate(binks_proportion = round(number_of_binks/number_of_cashes * 100, 2)) %>%
+  select(name = new_name, nationality = new_nationality, average_buy_in, 
+         number_of_cashes, sum_of_cashes, average_cash, average_placement, number_of_binks,
+         binks_proportion, number_of_countries_cashed, first_date, last_date, years_played, 
+         average_time_btwn_cash, unique_views) %>%
+  arrange(desc(sum_of_cashes)) %>%
+  mutate(quantile = ntile(sum_of_cashes, 4)) 
+
+#average time between cash interpreted as "he has a cash once every X days on average"
+
+first_hendon_mob_quantile <- hendon_summaries_df %>%
+  filter(quantile == 1)
+
+second_hendon_mob_quantile <- hendon_summaries_df %>%
+  filter(quantile == 2)
+
+third_hendon_mob_quantile <- hendon_summaries_df %>%
+  filter(quantile == 3)
+
+fourth_hendon_mob_quantile <- hendon_summaries_df %>%
+  filter(quantile == 4)
+
+
+
+# Nationalities in the dataset --------------------------------------------
+
+
+nationality_summary <- hendon_summaries_df %>%
+  group_by(nationality) %>%
+  summarise(number = n()) %>%
+  arrange(desc(number)) %>%
+  top_n(10, number)
+
+top_nationality_plot <- ggplot(data=nationality_summary, aes(x=reorder(nationality, -number), y = number))
+
+top_nationality_plot + geom_bar(stat = "identity", fill = "cadetblue2", col = "orangered1" ) + 
+  labs(x = "Nationality", y = "Number of Players", 
+       title = "Top 10 Nationalities of 1,000 Randomly Sampled Poker Players", caption = "Source: The Hendon Mob") +
+  theme_classic() + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(axis.text.x = element_text(angle=90, hjust=1, size = 10)) 
+
+
+
+# Distribution of unique views --------------------------------------------
+
+range(hendon_summaries_df$unique_views)
+
+unique_views_plot <- ggplot(hendon_summaries_df, aes(unique_views))
+
+unique_views_plot + 
+  theme_bw() + 
+  geom_histogram(breaks=seq(0, 15000, by=1500), col="red", fill="steelblue", alpha = 1) + 
+  scale_x_continuous(breaks = seq(0,15000, by = 1500), limits = c(0,17500)) + 
+  labs(x = "Number of Views", y = "Number of Players", title = "Distribution of Unique Views per Player") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+# Relationship between views and cashes -----------------------------------
+
+range(hendon_summaries_df$sum_of_cashes)
+
+cor(hendon_summaries_df$sum_of_cashes, hendon_summaries_df$unique_views, method = "pearson") 
+
+cashes_and_views_plot <- ggplot(hendon_summaries_df, aes(x = sum_of_cashes, y = unique_views))
+
+cashes_and_views_plot + 
+  theme_bw() +
+  geom_point(col = "blue", alpha = .5) +
+  scale_x_continuous(breaks = seq(0,10000000, by = 1000000), limits = c(0,11000000)) +
+  labs(x = "Live Earnings", y = "Unique Views", title = "Relationship between Live Earnings and Unique Views on Hendon Mob", caption = "Source: The Hendon Mob") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_smooth(method = "lm") +
+  annotate("text", x = 1200000, y = 30000, label = "R = 0.86")
+
+
+cashes_and_views_plot + 
+  theme_bw() +
+  geom_point(col = "blue", alpha = .5) +
+  scale_x_continuous(breaks = seq(0,200000, by = 10000), limits = c(0,210000)) +
+  labs(x = "Live Earnings", y = "Unique Views", title = "Relationship between Live Earnings and Unique Views on Hendon Mob", caption = "Source: The Hendon Mob") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_smooth(method = "lm")
+
+ 
+#slope of line gets smaller as we reduce live earnings. Unique views increase more as you increase live earnings,
+#somewhat exponential growth.
+
+
+# Unique views per country ------------------------------------------------
+
+
+unique_views_country_summary <- hendon_summaries_df %>%
+  group_by(nationality) %>%
+  summarise(average_unique_views = mean(unique_views), number_of_players = n())
+
+
+unique_views_country_summary_top_10 <- unique_views_country_summary %>%
+  top_n(10, number_of_players)
+
+#average unique views for top 10 countries by number_of_players
+
+average_unique_views_plot <- ggplot(data = unique_views_country_summary_top_10 , aes(x=reorder(nationality, - average_unique_views), y = average_unique_views))
+
+average_unique_views_plot + geom_bar(stat = "identity", fill = "steelblue", col = "orange") + 
+  labs(x = "Nationality of Player", y = "Average Number of Views per Player", 
+       title = "Average Number of Views for Players by Nationality (Top 10 Countries by # of Players only)", subtitle = "People search German players most often on average", caption = "Source: The Hendon Mob") +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(plot.subtitle = element_text(hjust = 0.5)) +
+  theme(axis.text.x = element_text(angle=90, hjust=1, size = 10)) 
+
+
+
+# Average Total Earnings per Country among Players ------------------------
+
+
+live_earnings_country_summary <- hendon_summaries_df %>%
+  group_by(nationality) %>%
+  summarise(average_live_earnings = mean(sum_of_cashes), number_of_players = n())
+
+
+live_earnings_country_summary_top_10 <- live_earnings_country_summary %>%
+  top_n(10, number_of_players)
+
+#average unique views for top 10 countries by number_of_players
+
+average_live_earnings_plot <- ggplot(data = live_earnings_country_summary_top_10 , aes(x=reorder(nationality, - average_live_earnings), y = average_live_earnings))
+
+average_live_earnings_plot + geom_bar(stat = "identity", fill = "aquamarine4", col = "darkorange2") + 
+  labs(x = "Nationality of Player", y = "Average Live Earnings per Player", 
+       title = "Average Live Earnings for Players by Nationality (Top 10 Countries by # of Players only)", subtitle = "German players have the highest earnings on average", caption = "Source: The Hendon Mob") +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(plot.subtitle = element_text(hjust = 0.5)) +
+  theme(axis.text.x = element_text(angle=90, hjust=1, size = 10)) 
+
+
+
+# Notes and possible analyses ---------------------------------------------
+
+#Ideas: 
+
+#1: Try to tell the story of what happens to players after a big score.
+#What is a big score? How many players experience a big score? What happens afterwards?
+#How many players follow the big score up with another big score? 
+#How many go on a huge drought?
+#Metrics needed: Definition of big score, usage of cashes summary in computation of this
+
+
+#2 Try to find what makes a player famous. Regression models for "unique views" including random forests, 
+#other machine learning decision tree models
+
+#x variables: live earnings, country, number of cashes, average placement, years played
+#y variable unique views
+
+
+#3 Look at distribution of sample of Hendon Mob players, turn it into a Markdown/Shiny project
+#and post on Github as creation of portfolio
+
+#Note ~ 600,000 players on hendon mob
+
+#1% sample is 6,000
+
+#Things to look at - 
+
+#Distribution of countries in DB
+#Distribution of total earnings
+#Distribution of number of outliers
+#Distribution of average time between cashes
+#Distribution of time between outlier and next cash
+#Disribution in amount of countries cashed in
+#Buy-ins? (This is difficult with Hendon Mob)
+#Cashes/year
+#What can we say and what can we not say with Hendon Mob Data??
+
+
+#Separate into groups
+#High, medium, low cashes
+#What do these populations look like with the parameters above?
+#Are there different average stories for each population?
+#Nature of differences between groups?
+
+#We are defining bink as any score more than 100x average buy in
+
+
+#Need a plan for analyses and how to present them in my markdown
+
+#Probably something about the entire population about the specific differences 
+#between quartiles - how do these categories/players differ
+
+
+# Distribution of total earnings ------------------------------------------
+
+display_dist_tot_earnings <- function(hendon_summaries){ 
+  
+  max_cash <- signif(max(hendon_summaries$sum_of_cashes), 2)
+  
+  total_earnings_plot <- ggplot(data = hendon_summaries, aes(sum_of_cashes))
+  
+  total_earnings_plot + 
+    theme_bw() + 
+    geom_histogram(breaks=seq(0, max_cash, by=max_cash/10), col="red", fill="steelblue", alpha = 1) + 
+    scale_x_continuous(breaks = seq(0,max_cash, by = max_cash/10), limits = c(0,max_cash)) + 
+    labs(x = "Total Live Earnings", y = "Number of Players", title = "Distribution of Live Earnings per Player") + 
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  
+}
+
+display_dist_tot_earnings(first_hendon_mob_quantile)
+display_dist_tot_earnings(second_hendon_mob_quantile)
+display_dist_tot_earnings(third_hendon_mob_quantile)
+display_dist_tot_earnings(fourth_hendon_mob_quantile)
+
+#Conclusion - live earnings are more and more right skewed in higher quartiles of players
+
+
+# Distribution of Amount of Countries Cashed in ---------------------------
+
+display_dist_countries_cashed <- function(hendon_summaries){
+  
+  max_countries_cashed <- max(hendon_summaries$number_of_countries_cashed)
+  
+  countries_cashed_plot <- ggplot(data = hendon_summaries, aes(number_of_countries_cashed))
+  
+  
+  countries_cashed_plot + 
+    theme_bw() + 
+    geom_histogram(breaks=seq(0, max_countries_cashed + 2, by=2), col="red", fill="steelblue", alpha = 1) + 
+    scale_x_continuous(breaks = seq(0,max_countries_cashed + 2, by = 2), limits = c(0,max_countries_cashed + 1)) + 
+    labs(x = "Number of Countries Cashed", y = "Number of Players", 
+         title = "Distribution of Number of Countries Cashed") + 
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  
+}
+
+display_dist_countries_cashed(hendon_summaries_df)
+display_dist_countries_cashed(first_hendon_mob_quantile)
+display_dist_countries_cashed(second_hendon_mob_quantile)
+display_dist_countries_cashed(third_hendon_mob_quantile)
+display_dist_countries_cashed(fourth_hendon_mob_quantile)
+
+
+earnings_and_countries_plot <- ggplot(hendon_summaries_df, aes(x = sum_of_cashes, y = number_of_countries_cashed))
+
+earnings_and_countries_plot  + 
+  theme_bw() +
+  geom_point(col = "blue", alpha = .5) +
+  scale_x_continuous(breaks = seq(0,10000000, by = 1000000), limits = c(0,11000000)) +
+  labs(x = "Live Earnings", y = "Countries Cashed", title = "Relationship between Live Earnings and Countries Cashed on Hendon Mob", caption = "Source: The Hendon Mob") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_smooth(method = "lm") 
+
+
+# Average Time between Cashes Distribution --------------------------------
+
+
+display_dist_time_btwn_cashes <- function(hendon_summaries){ 
+  
+  hendon_summaries <- hendon_summaries %>%
+    filter(number_of_cashes > 1)
+  
+  time_cashes_plot <- ggplot(data = hendon_summaries, aes(average_time_btwn_cash))
+  
+  max_time <- signif(max(hendon_summaries$average_time_btwn_cash), 2)
+  
+  time_cashes_plot + 
+    theme_bw() + 
+    geom_histogram(breaks=seq(0, max_time, by=max_time/10), col="red", fill="steelblue", alpha = 1) + 
+    scale_x_continuous(breaks = seq(0,max_time, by = max_time/10), limits = c(0,max_time)) + 
+    labs(x = "Time between Cashes (days)", y = "Number of Players", title = "Distribution of Days Between Cashes") + 
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  
+}
+
+display_dist_time_btwn_cashes(hendon_summaries_df)
+display_dist_time_btwn_cashes(first_hendon_mob_quantile)
+display_dist_time_btwn_cashes(second_hendon_mob_quantile)
+display_dist_time_btwn_cashes(third_hendon_mob_quantile)
+display_dist_time_btwn_cashes(fourth_hendon_mob_quantile)
+
+#people with more cashes play more often?
+
+#average time for each quartile -
+
+
+hendon_summaries_more_one_cash <- hendon_summaries_df %>%
+  filter(number_of_cashes > 1)
+
+hendon_summaries_average_time_between <- mean(hendon_summaries_more_one_cash$average_time_btwn_cash)
+
+first_hendon_mob_quantile_more_one_cash <- first_hendon_mob_quantile %>%
+  filter(number_of_cashes > 1)
+
+first_hendon_mob_quantile_average_time_between <- mean(first_hendon_mob_quantile$average_time_btwn_cash)
+
+second_hendon_mob_quantile_more_one_cash <- second_hendon_mob_quantile_df %>%
+  filter(number_of_cashes > 1)
+
+second_hendon_mob_quantile_average_time_between <- mean(second_hendon_mob_quantile$average_time_btwn_cash)
+
+third_hendon_mob_quantile_more_one_cash <- third_hendon_mob_quantile_df %>%
+  filter(number_of_cashes > 1)
+
+third_hendon_mob_quantile_quantile_average_time_between <- mean(third_hendon_mob_quantile$average_time_btwn_cash)
+
+fourth_hendon_mob_quantile_more_one_cash <- fourth_hendon_mob_quantile_df %>%
+  filter(number_of_cashes > 1)
+
+fourth_hendon_mob_quantile_average_time_between <- mean(fourth_hendon_mob_quantile$average_time_btwn_cash)
+
+time_between_vec <- c(first_hendon_mob_quantile_average_time_between, second_hendon_mob_quantile_average_time_between,
+                      third_hendon_mob_quantile_quantile_average_time_between, fourth_hendon_mob_quantile_average_time_between)
+
+time_between_df <- tibble(quartile = seq(1:4), average_time = time_between_vec)
+
+time_between_plot <- ggplot(time_between_df, aes(quartile, average_time))
+
+time_between_plot +
+  theme_bw() +
+  geom_bar(stat = "identity", col = "red", fill = "steelblue") +
+  labs(x = "Quartile", y = "Average Time Between Cashes (days)", title = "Average Time Between Cashes", caption = "Source: The Hendon Mob") + 
+  theme(plot.title = element_text(hjust = 0.5)) 
+
+#People with more cashes have longer between them
+
+# Testing -----------------------------------------------------------------
+
+max_time <- max(hendon_summaries_df$average_time_btwn_cash)
+
